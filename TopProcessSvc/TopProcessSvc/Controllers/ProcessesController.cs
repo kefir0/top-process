8fe254c2d619a085847cd128f0cbf28c7ab63f08
nAXAH1@gmail.com <nAXAH1@gmail.com@128807ac-9dfd-e4c7-8f23-54a6b16ffa15>
2015-01-13 19:44:30 +0000

    Initial commit

---
+++ TopProcessSvc/TopProcessSvc/Controllers/ProcessesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using TopProcessSvc.Models;
+
+namespace TopProcessSvc.Controllers
+{
+    public class ProcessesController : ApiController
+    {
+        //
+        // GET: /Processes/
+
+        public IEnumerable<Process> Get()
+        {
+            yield return new Process {Name = "system", CpuUsage = 13, Id = 1};
+            yield return new Process {Name = "svchost.exe", CpuUsage = 23, Id = 20};
+        }
+    }
+}
