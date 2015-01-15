ba3bec0dc3252c9c2c6f780d8a54adc0aa8c7615
nAXAH1@gmail.com <nAXAH1@gmail.com@128807ac-9dfd-e4c7-8f23-54a6b16ffa15>
2015-01-15 12:38:55 +0000

    Initial ajax loading

---
+++ TopProcessSvc/TopProcessSvc/Models/ProcessesInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace TopProcessSvc.Models
+{
+    /// <summary>
+    /// Server processes DTO.
+    /// </summary>
+    public class ProcessesInfo
+    {
+        /// <summary>
+        /// Gets or sets the name of the server.
+        /// </summary>
+        public string ServerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the processes.
+        /// </summary>
+        public IEnumerable<ProcessInfo> Processes { get; set; }
+    }
+}
---
+++ TopProcessSvc/TopProcessSvc/WebInterface/scripts/controllers.js
@@ -0,0 +1,28 @@
+﻿var topProcessApp = angular.module('topProcessApp', []);
+
+topProcessApp.controller('MainController', function ($scope, $http) {
+    $scope.loading = true;
+    $scope.processFilter = '';
+
+    $scope.LoadBranches = function () {
+        $http.get("/api/Processes")
+            .success(function (data) {
+                $scope.error = "";
+                $scope.loading = false;
+                $scope.processes = data;
+            })
+            .error(function (err) {
+                $scope.loading = false;
+                $scope.error = "Failed to load processes";
+                $scope.errorDetails = err;
+            });
+    };
+
+    $scope.processesGrid = {
+        data: 'processes.Processes | filter:processFilter',
+        enableColumnResize: true,
+        enableRowSelection: true
+    };
+
+    $scope.LoadBranches();
+});
