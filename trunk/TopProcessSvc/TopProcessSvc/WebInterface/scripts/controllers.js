b944b149b0d15ba167832c4e1f97f81ca8b7091e
nAXAH1@gmail.com <nAXAH1@gmail.com@128807ac-9dfd-e4c7-8f23-54a6b16ffa15>
2015-01-16 18:17:48 +0000

    Memory chart attempt

--- trunk/TopProcessSvc/TopProcessSvc/WebInterface/scripts/controllers.js
+++ trunk/TopProcessSvc/TopProcessSvc/WebInterface/scripts/controllers.js
@@ -1,4 +1,4 @@
-﻿var topProcessApp = angular.module('topProcessApp', ['ngGrid']);
+﻿var topProcessApp = angular.module('topProcessApp', ['ngGrid', 'highcharts-ng']);
 
 topProcessApp.controller('MainController', function ($scope, $http) {
     $scope.loading = true;
@@ -9,7 +9,11 @@ topProcessApp.controller('MainController', function ($scope, $http) {
             .success(function (data) {
                 $scope.error = "";
                 $scope.loading = false;
+
                 $scope.processes = data;
+                $scope.memoryChart.series[0].data.push(data.MemoryUsed);
+                $scope.memoryChart.yAxis.max = data.MemoryTotal;
+
                 setTimeout(function () {
                     $(document).resize();
                 }, 300); // fix grid columns
@@ -26,6 +30,64 @@ topProcessApp.controller('MainController', function ($scope, $http) {
         enableColumnResize: true,
         enableRowSelection: true,
         //plugins: [new ngGridFlexibleHeightPlugin()]
+    };
+
+    $scope.memoryChart = {
+        chart: {
+            type: 'area'
+        },
+        title: {
+            text: 'Memory Usage'
+        },
+        xAxis: {
+            allowDecimals: false,
+            labels: {
+                formatter: function() {
+                    return this.value;
+                }
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Used Memory'
+            },
+            labels: {
+                formatter: function() {
+                    return this.value/1024/1024/1024 + 'GB';
+                }
+            },
+        },
+        //options: {
+        //    yAxis: [
+        //        {
+        //            min: 0,
+        //            max: 1024 * 1024 * 1024
+        //        }
+        //    ],
+        //},
+        tooltip: {
+            pointFormat: '{point.y/1024/1024} MB'
+        },
+        plotOptions: {
+            area: {
+                marker: {
+                    enabled: false,
+                    symbol: 'circle',
+                    radius: 2,
+                    states: {
+                        hover: {
+                            enabled: true
+                        }
+                    }
+                }
+            }
+        },
+        series: [
+            {
+                name: 'Used Memory',
+                data: [100,200,300,1204*1024*1024]
+            }
+        ]
     };
 
     $scope.LoadBranches();
