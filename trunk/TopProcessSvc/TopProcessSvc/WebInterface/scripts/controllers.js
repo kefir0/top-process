4acfe52ad033503c61beaf2ef02946f80cb5e527
nAXAH1@gmail.com <nAXAH1@gmail.com@128807ac-9dfd-e4c7-8f23-54a6b16ffa15>
2015-01-17 11:07:52 +0000

    Auto-refresh works

--- trunk/TopProcessSvc/TopProcessSvc/WebInterface/scripts/controllers.js
+++ trunk/TopProcessSvc/TopProcessSvc/WebInterface/scripts/controllers.js
@@ -3,18 +3,14 @@
 topProcessApp.controller('MainController', function ($scope, $http) {
     $scope.loading = true;
     $scope.processFilter = '';
+    $scope.autoRefresh = true;
 
-    $scope.LoadBranches = function () {
+    $scope.loadBranches = function () {
         $http.get("../api/Processes")
             .success(function (data) {
                 $scope.error = "";
                 $scope.loading = false;
-
                 $scope.processes = data;
-
-                setTimeout(function () {
-                    $(document).resize();
-                }, 300); // fix grid columns
             })
             .error(function (err) {
                 $scope.loading = false;
@@ -23,11 +19,18 @@ topProcessApp.controller('MainController', function ($scope, $http) {
             });
     };
 
+    $scope.refreshInfo = function () {
+        if ($scope.autoRefresh) {
+            $scope.loadBranches();
+        }
+        setTimeout($scope.refreshInfo, 2000);
+    }
+
     $scope.processesGrid = {
         data: 'processes.Processes',
         enableColumnResize: true,
         enableRowSelection: true,
     };
 
-    $scope.LoadBranches();
+    $scope.refreshInfo();
 });
